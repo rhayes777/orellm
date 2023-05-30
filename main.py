de8bdbcd2be7a8e41dc6f694757cc25ad3943fe6
Richard <richard@rghsoftware.co.uk>
2023-05-30 17:32:39 +0100

    fix

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from orellm import complete_object
-from orellm.class_ import Class
 from orellm.example_types import Types
 from orellm.collection import List
 
 cls = List(Types)
 
+print(cls.regex)
+
 model = AutoModelForCausalLM.from_pretrained("gpt2")
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
-prompt = "Give me a Types"
+prompt = "Give a list of three Types"
 
 output = complete_object(
     tokenizer=tokenizer,
